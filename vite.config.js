730d1526142407f8f2aa30a8a930a60043066719
Joydeep Kar <joydeepkar16@gmail.com>
2025-07-27 18:19:15 +0530

    Fix comma in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sudoku-react-game/'
+  base: '/sudoku-react-game/',
   plugins: [react()],
 })
